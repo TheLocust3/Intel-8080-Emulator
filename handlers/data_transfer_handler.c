0b271d390c1a7756c7c316ad28368a6c4c9a3bcd
TheLocust3 <jake.kinsella@gmail.com>
2017-04-18 21:01:37 -0400

    Cleanup, fix move to memory immediate handling, and skeleton for LXI

--- handlers/data_transfer_handler.c
+++ handlers/data_transfer_handler.c
@@ -28,19 +28,13 @@ bool handle_MOV(uint8_t instruction)
         uint8_t src_register = get_register_from_code(src_code);
 
         if (dst_register != NULL_REGISTER && src_register != NULL_REGISTER) {
-            printf("MOV r1, r2\n");
             move_register(&dst_register, src_register);
-
             return true;
         } else if (dst_register != NULL_REGISTER && src_register == NULL_REGISTER) {
-            printf("MOV r, M\n");
             move_from_memory(&dst_register);
-
             return true;
         } else if (dst_register == NULL_REGISTER && src_register != NULL_REGISTER) {
-            printf("MOV M, r\n");
             move_to_memory(src_register);
-
             return true;
         }
     }
@@ -51,10 +45,8 @@ bool handle_MOV(uint8_t instruction)
 bool handle_MVI(uint8_t instruction)
 {
     if (((instruction & 0b11000000) >> 6) == 0b00 && (instruction & 0b00000111) == 0b110) {
-        if ((instruction & 0b00111000) == 0b110) {
-            printf("MVI M, data");
+        if ((instruction & 0b00111000) >> 3 == 0b110) {
             move_to_memory_immediate();
-
             return true;
         }
 
@@ -62,9 +54,7 @@ bool handle_MVI(uint8_t instruction)
         uint8_t dst_register = get_register_from_code(dst_code);
 
         if (dst_register != NULL_REGISTER) {
-            printf("MVI r, data\n");
             move_immediate(&dst_register);
-
             return true;
         }
     }
--- opcodes/data_transfer.c
+++ opcodes/data_transfer.c
@@ -15,18 +15,24 @@ void move_byte_to_memory(const uint16_t dst_address, const uint8_t src);
 
 void move_register(uint8_t *dst_register, const uint8_t src_register)
 {
+    printf("MOV r1, r2\n");
+
     move_byte(dst_register, src_register);
     pc++;
 }
 
 void move_from_memory(uint8_t *dst_register)
 {
+    printf("MOV r, M\n");
+
     move_byte(dst_register, read_byte(h, l));
     pc++;
 }
 
 void move_to_memory(const uint8_t src_register)
 {
+    printf("MOV M, r\n");
+
     move_byte_to_memory(combine_bytes(h, l), src_register);
     pc++;
 }
@@ -35,14 +41,25 @@ void move_to_memory(const uint8_t src_register)
 
 void move_immediate(uint8_t *dst_register)
 {
+    printf("MVI r, data\n");
+
     move_byte(dst_register, read_byte_from_address((uint16_t) (pc + 1)));
     pc += 2;
 }
 
 void move_to_memory_immediate()
 {
+    printf("MVI M, data\n");
+
     move_byte_to_memory(combine_bytes(h, l), read_byte_from_address((uint16_t) (pc + 1)));
     pc += 2;
+}
+
+// Misc
+
+void load_register_pair_immediate()
+{
+    printf("LXI rp, data 16");
 }
 
 void move_byte(uint8_t *dst, const uint8_t src)

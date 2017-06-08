3db6df5650f9c4af3340ad34243452aadc4712a4
TheLocust3 <jake.kinsella@gmail.com>
2017-06-08 14:59:20 -0400

    Setup parser for move commands

--- main.c
+++ main.c
@@ -11,12 +11,13 @@
 #include "matcher/instruction_template.h"
 #include "matcher/matcher.h"
 
+void setup_opcodes();
+
 int main(int argc, const char* argv[])
 {
+    setup_opcodes();
     initialize();
 
-    InstructionTemplate instruction_template = new_instruction_template("01DDDSSS", &move_register);
-    add_instruction_template(instruction_template);
     printf("%d\n", match(0b01111000));
 
     set_byte(0, 0, 0b00111010);
@@ -30,4 +31,27 @@ int main(int argc, const char* argv[])
     }
 
     return 0;
+}
+
+void setup_opcodes() {
+    InstructionTemplate move_register_template = new_instruction_template("01DDDSSS", &move_register);
+    add_instruction_template(move_register_template);
+
+    InstructionTemplate move_from_memory_template = new_instruction_template("01DDD110", &move_from_memory);
+    add_instruction_template(move_from_memory_template);
+
+    InstructionTemplate move_to_memory_template = new_instruction_template("01110SSS", &move_to_memory);
+    add_instruction_template(move_to_memory_template);
+
+    InstructionTemplate move_immediate_template = new_instruction_template("00DDD110", &move_immediate);
+    add_instruction_template(move_immediate_template);
+
+    InstructionTemplate move_to_memory_immediate_template = new_instruction_template("00110110", &move_to_memory_immediate);
+    add_instruction_template(move_to_memory_immediate_template);
+
+    InstructionTemplate load_register_pair_immediate_template = new_instruction_template("00110110", &load_register_pair_immediate);
+    add_instruction_template(load_register_pair_immediate_template);
+
+    InstructionTemplate load_accumulator_direct_template = new_instruction_template("00110110", &load_accumulator_direct);
+    add_instruction_template(load_accumulator_direct_template);
 }

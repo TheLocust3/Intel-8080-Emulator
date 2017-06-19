447af76c29ce816cfd6ac48f2bc6c89ce3be325f
TheLocust3 <jake.kinsella@gmail.com>
2017-06-18 21:31:41 -0400

    Add dst, src, and rp run tests

--- tests/matcher_run_tests.c
+++ tests/matcher_run_tests.c
@@ -5,24 +5,81 @@
 #include <assert.h>
 #include "../src/matcher/matcher.h"
 
+void setup();
 void flip_bool();
+
 void simple_run_test();
+void dst_run_test();
+void src_run_test();
+void rp_run_test();
+void dst_and_src_run_test();
 
 bool has_flipped = false;
 
 int main(int argc, const char* argv[])
 {
     simple_run_test();
+    dst_run_test();
+    src_run_test();
+    rp_run_test();
+    dst_and_src_run_test();
 
     return 0;
 }
 
 void simple_run_test()
 {
-    clear_all_registered_templates();
-    has_flipped = false;
+    setup();
 
     InstructionTemplate template = new_instruction_template("00000000", &flip_bool);
+    add_instruction_template(template);
+
+    run_instruction(0, 0, 0, template);
+
+    assert(has_flipped && "Template function was not run");
+}
+
+void dst_run_test()
+{
+    setup();
+
+    InstructionTemplate template = new_instruction_template("00DDD000", &flip_bool);
+    add_instruction_template(template);
+
+    run_instruction(0, 0, 0, template);
+
+    assert(has_flipped && "Template function was not run");
+}
+
+void src_run_test()
+{
+    setup();
+
+    InstructionTemplate template = new_instruction_template("00000SSS", &flip_bool);
+    add_instruction_template(template);
+
+    run_instruction(0, 0, 0, template);
+
+    assert(has_flipped && "Template function was not run");
+}
+
+void rp_run_test()
+{
+    setup();
+
+    InstructionTemplate template = new_instruction_template("00RP0000", &flip_bool);
+    add_instruction_template(template);
+
+    run_instruction(0, 0, 0, template);
+
+    assert(has_flipped && "Template function was not run");
+}
+
+void dst_and_src_run_test()
+{
+    setup();
+
+    InstructionTemplate template = new_instruction_template("00DDDSSS", &flip_bool);
     add_instruction_template(template);
 
     run_instruction(0, 0, 0, template);
@@ -33,4 +90,10 @@ void simple_run_test()
 void flip_bool()
 {
     has_flipped = true;
+}
+
+void setup()
+{
+    clear_all_registered_templates();
+    has_flipped = false;
 }

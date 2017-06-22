c4e336db4ac4b88fbd6d3b29c14dc28409906259
TheLocust3 <jake.kinsella@gmail.com>
2017-06-22 18:04:39 -0400

    Make NOP test

---
+++ tests/opcodes/miscellaneous_tests.c
@@ -0,0 +1,24 @@
+//
+// Created by Jake Kinsella on 6/22/17.
+//
+
+#include <assert.h>
+#include "../../src/opcodes/miscellaneous.h"
+#include "../../src/registers.h"
+
+void nop_test();
+
+int main(int argc, const char* argv[])
+{
+    nop_test();
+
+    return 0;
+}
+
+void nop_test()
+{
+    pc = 0;
+    nop();
+
+    assert(pc == 1 && "NOP isn't implemented properly");
+}

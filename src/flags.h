82707b3942add8a8a90a64e8dd4615622893700d
TheLocust3 <jake.kinsella@gmail.com>
2017-06-24 17:20:27 -0400

    Update flag header

--- src/flags.h
+++ src/flags.h
@@ -7,4 +7,10 @@
 
 #include "registers.h"
 
+void set_zero_flag(bool flag);
+void set_sign_flag(bool flag);
+void set_parity_flag(bool flag);
+void set_carry_flag(bool flag);
+void set_aux_carry_flag(bool flag);
+
 #endif //INC_8080_EMULATOR_FLAGS_H

2adc6190d5f822f289033bd30837c715ba726865
TheLocust3 <jake.kinsella@gmail.com>
2017-07-13 18:41:42 -0400

    Add tests for conditional jumps

--- tests/opcodes/branch/jump_tests.c
+++ tests/opcodes/branch/jump_tests.c
@@ -6,14 +6,31 @@
 #include "../../../src/registers.h"
 #include "../../../src/ram.h"
 #include "../../../src/opcodes/branch/jump.h"
+#include "../../../src/flags.h"
 
 void jump_test();
+void jump_not_zero_test();
+void jump_zero_test();
+void jump_not_carry_test();
+void jump_carry_test();
+void jump_not_parity_test();
+void jump_parity_test();
+void jump_not_sign_test();
+void jump_sign_test();
 
 const int TEST_ADDRESS = 10;
 
 int main(int argc, const char* argv[])
 {
     jump_test();
+    jump_not_zero_test();
+    jump_zero_test();
+    jump_not_carry_test();
+    jump_carry_test();
+    jump_not_parity_test();
+    jump_parity_test();
+    jump_not_sign_test();
+    jump_sign_test();
 
     return 0;
 }
@@ -28,4 +45,115 @@ void jump_test()
     jump();
 
     assert(pc == 10 && "jump_test failed!");
+}
+
+void jump_not_zero_test()
+{
+    pc = 0;
+    f = 0;
+    set_zero_flag(false);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_NZ);
+
+    assert(pc == 10 && "jump_not_zero_test failed!");
+}
+
+void jump_zero_test()
+{
+    pc = 0;
+    set_zero_flag(true);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_Z);
+
+    assert(pc == 10 && "jump_zero_test failed!");
+}
+
+void jump_not_carry_test()
+{
+    pc = 0;
+    f = 0;
+    set_carry_flag(false);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_NC);
+
+    assert(pc == 10 && "jump_not_carry_test failed!");
+}
+
+void jump_carry_test()
+{
+    pc = 0;
+    f = 0;
+    set_carry_flag(true);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_C);
+
+    assert(pc == 10 && "jump_carry_test failed!");
+}
+
+void jump_not_parity_test()
+{
+    pc = 0;
+    f = 0;
+    set_parity_flag(false);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_PO);
+
+    assert(pc == 10 && "jump_not_parity_test failed!");
+}
+
+void jump_parity_test()
+{
+    pc = 0;
+    f = 0;
+    set_parity_flag(true);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_PE);
+
+    assert(pc == 10 && "jump_parity_test failed!");
+}
+
+void jump_not_sign_test()
+{
+    pc = 0;
+    f = 0;
+    set_sign_flag(false);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_P);
+
+    assert(pc == 10 && "jump_not_sign_test failed!");
+}
+
+void jump_sign_test()
+{
+    pc = 0;
+    f = 0;
+    set_sign_flag(true);
+
+    set_byte_from_address((uint16_t) (pc + 1), (uint8_t) TEST_ADDRESS);
+    set_byte_from_address((uint16_t) (pc + 2), 0);
+
+    jump_condition(CONDITIONAL_M);
+
+    assert(pc == 10 && "jump_sign_test failed!");
 }

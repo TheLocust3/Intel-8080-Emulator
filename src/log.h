5ee490211128d9b87e570e8a3917ea28cc3aaa38
TheLocust3 <jake.kinsella@gmail.com>
2017-07-30 20:53:35 -0400

    Cleanup log function and add log level

--- src/log.h
+++ src/log.h
@@ -5,6 +5,11 @@
 #ifndef INC_8080_EMULATOR_LOG_H
 #define INC_8080_EMULATOR_LOG_H
 
+#include <printf.h>
+#include <stdbool.h>
+
+#define LOG true
+
 void log(char *fmt, ...);
 
 #endif //INC_8080_EMULATOR_LOG_H
